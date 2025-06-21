d861bc94414c951bc2365f69e99cb79920309f74
Whitenessly <whitenessly@gmail.com>
2025-06-21 20:09:57 +0700

    ..

--- src/Components/Anime/Anime.jsx
+++ src/Components/Anime/Anime.jsx
@@ -8,7 +8,7 @@ const Anime = (props) => {
 
     return (
         <div onClick={handleClick} className='w-[13%] flex flex-col gap-[5px] items-center'>
-            <div className='w-full relative'>
+            <div className='w-full relative rounded-xl overflow-hidden'>
                 <img src={props.item.image} className='w-full' alt="" />
                 <div className='w-full h-full absolute top-0 bg-gradient-to-t from-[#1D1D1DCC] flex items-end justify-center p-[7px]'>
                     <p className='text-white text-bafe font-normal'>Episode {props.item.episode}</p>
